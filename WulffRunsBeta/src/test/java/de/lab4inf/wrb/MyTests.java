3a7b955defdbd66a1fef2ddfe0c34d274ebdc780
aBlackCat <aBlackCat@AthenaControl.fritz.box>
2018-11-11 00:17:17 +0100

    3 moar tests
    - just to get that sweet sweet coverage

--- WulffRunsBeta/src/test/java/de/lab4inf/wrb/MyTests.java
+++ WulffRunsBeta/src/test/java/de/lab4inf/wrb/MyTests.java
@@ -13,10 +13,22 @@ public class MyTests extends AbstractScriptTest {
 	}
 
 
-//	@Test
-//    public final void testnewPlus() throws Exception {
-//        String task = "2+3";
-//        assertEquals(5.0, script.parse(task), eps);
-//    }
+	@Test
+    public final void testMod() throws Exception {
+        String task = "12%5";
+        assertEquals(2.0, script.parse(task), EPS);
+    }
+	
+	@Test
+    public final void testTanh() throws Exception {
+        String task = "tanh(3)";
+        assertEquals(Math.tanh(3), script.parse(task), EPS);
+    }
+	
+	@Test(expected = IllegalArgumentException.class)
+    public final void testVar() throws Exception {
+        String task = "a + 2";
+        assertEquals(2.0, script.parse(task), EPS);
+    }
 
 }
