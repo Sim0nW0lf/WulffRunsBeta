565a07ed5f6c3ab67003535b7df1f7a40164c531
iPwnWolf <simonjoelwolf@yahoo.com>
2018-11-11 00:26:56 +0100

    HAU REIN BRUDA

--- WulffRunsBeta/src/test/java/de/lab4inf/wrb/MyTests.java
+++ WulffRunsBeta/src/test/java/de/lab4inf/wrb/MyTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-
 public class MyTests extends AbstractScriptTest {
 
 	@Override
@@ -13,10 +12,29 @@ public class MyTests extends AbstractScriptTest {
 	}
 
 
-//	@Test
-//    public final void testnewPlus() throws Exception {
-//        String task = "2+3";
-//        assertEquals(5.0, script.parse(task), eps);
-//    }
+	@Test
+    public final void test_tanh() throws Exception {
+		double x = AbstractScriptTest.rnd();
+		script.setVariable("x", x);
+        String task = "tanh(x)";
+        assertEquals(Math.tanh(x), script.parse(task), EPS);
+    }
+	@Test
+    public final void testMod() throws Exception {
+        String task = "12%5";
+        assertEquals(2.0, script.parse(task), EPS);
+    }
+	
+	@Test
+    public final void testTanh() throws Exception {
+        String task = "tanh(3)";
+        assertEquals(Math.tanh(3), script.parse(task), EPS);
+    }
+	
+	@Test(expected = IllegalArgumentException.class)
+    public final void testVar() throws Exception {
+        String task = "a + 2";
+        assertEquals(2.0, script.parse(task), EPS);
+    }
 
 }
